3a668099cdd8938b2f1d1a8a6501f3bcf2860923
ko1358 <hiroeabe134@gmail.com>
2024-03-15 22:49:59 +0900

    四択変更しました

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,21 +1,22 @@
 <template>
   <div>
-    <p>これはサンプルのコードなので適宜書き換えてください</p>
+    <p>ハムハムちゃん！</p>
     <Question
-      v-if="!gameOver && !gameWon"
-      :question="currentQuestion"
+     v-if="!gameOver && !gameWon"
+      :question="currentQuestion.question"
+      :choices="currentQuestion.choices"
       @answer="checkAnswer"
     />
     <Result
       v-else-if="gameOver || gameWon"
       :gameOver="gameOver"
+      :gameWon="gameWon"
       @restart="restartGame"
     />
   </div>
 </template>
 
 <script>
-// これはサンプルのコードなので適宜書き換えてください
 import Question from "./Question.vue";
 import Result from "./Result.vue";
 
@@ -27,9 +28,27 @@ export default {
   data() {
     return {
       stages: [
-        { choices: ["A", "B", "C", "D"], correctAnswers: 3 },
-        { choices: ["A", "B", "C", "D"], correctAnswers: 2 },
-        { choices: ["A", "B", "C", "D"], correctAnswers: 1 },
+        { question: "3人が降りてくれる選択肢を選ぼう", choices: [
+            { text: "降りて欲しそうに見つめる", isCorrect: false },
+            { text: "突撃する", isCorrect: true },
+            { text: "「乗りたいんですけど〜」と言う", isCorrect: false },
+            { text: "ため息をつく", isCorrect: false }
+          ]
+        },
+        { question: "3人が降りてくれる選択肢を選ぼう", choices: [
+            { text: "「小声ですみませんと言う」", isCorrect: true },
+            { text: "「あの、15分ぐらい見送ってまして、、」と言う", isCorrect: false },
+            { text: "舌打ちする", isCorrect: false },
+            { text: "譲ってもらえるまで待ってみる", isCorrect: false }
+          ]
+        },
+        { question: "3人が降りてくれる選択肢を選ぼう", choices: [
+            { text: "「歩ける方、時間に余裕のある方はエスカレーターで行ってもらえませんか？」と言う", isCorrect: true },
+            { text: "仁義を切る", isCorrect: false },
+            { text: "「あ、UFOだ！」と言う", isCorrect: false },
+            { text: "「外国ではみんな譲ってくれるんだけどな〜」と大きな独り言を言う", isCorrect: false }
+          ]
+        },
       ],
       currentStageIndex: 0,
       correctAnswers: 0,
@@ -44,21 +63,16 @@ export default {
       return this.wrongAnswers > 0;
     },
     gameWon() {
-      return (
-        this.correctAnswers ===
-        this.stages.reduce((acc, stage) => acc + stage.correctAnswers, 0)
-      );
+      return this.correctAnswers === this.stages.length;
     },
   },
   methods: {
-    checkAnswer(answer) {
-      if (answer === this.currentQuestion.correctAnswers) {
+    checkAnswer(answerIndex) {
+      if (this.currentQuestion.choices[answerIndex].isCorrect) {
         this.correctAnswers++;
+        this.moveToNextStage();
       } else {
         this.wrongAnswers++;
-      }
-      if (this.wrongAnswers === 0) {
-        this.moveToNextStage();
       }
     },
     moveToNextStage() {
