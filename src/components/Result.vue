ad2c5a49ff90df860ee1499ab4c4c586b5cd37b3
konapeso <n2kgt1@gmail.com>
2024-03-19 22:34:40 +0900

    エラーの解消

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,21 +1,26 @@
 <template>
   <div class="button-container">
     <div v-if="gameOver">
-
-<div class="image-container">
-      <img  class="background-fail" src="../imgs/fail2.png" alt="" />
-      <p>ゲームオーバー！</p>
-      <button @click="restart" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-        もう一度
-      </button>
+      <div class="image-container">
+        <img class="background-fail" src="../imgs/fail2.png" alt="" />
+        <p>ゲームオーバー！</p>
+        <button
+          @click="restart"
+          class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+        >
+          もう一度
+        </button>
+      </div>
     </div>
   </div>
-</div>
-    <div v-if="gameWon">
-          <div class="image-container">
-      <img  class="background-clear" src="../imgs/clear.png" alt="" />
+  <div v-if="gameWon">
+    <div class="image-container">
+      <img class="background-clear" src="../imgs/clear.png" alt="" />
       <p>クリア！</p>
-      <button @click="restart" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+      <button
+        @click="restart"
+        class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+      >
         再スタート
       </button>
     </div>
@@ -27,26 +32,25 @@ export default {
   props: {
     gameWon: {
       type: Boolean,
-      required: true
+      required: true,
     },
     gameOver: {
       type: Boolean,
-      required: true
-    }
+      required: true,
+    },
   },
   methods: {
     restart() {
-      this.$emit('restart');
-    }
-  }
+      this.$emit("restart");
+    },
+  },
 };
 </script>
-
 
 <style scoped>
 .image-container {
   /* 画像のパスを指定 */
-  background-image: url('../imgs/0-start.png');
+  background-image: url("../imgs/0-start.png");
   /* 画像を中央上部に配置 */
   background-position: center top;
   /* 画像を1回だけ表示 */
@@ -57,6 +61,7 @@ export default {
   background-attachment: fixed;
   /* コンテナの高さを設定（必要に応じて） */
   height: 100vh;
+}
 
 .button-container {
   display: flex;
--- src/components/StartPage.vue
+++ src/components/StartPage.vue
@@ -1,56 +1,45 @@
 <template>
-    <div class="button-container">
-      <h1>ハムハムちゃん！</h1>
-      <router-link to="/game">
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          スタート
-        </button>
-      </router-link>
-    </div>
-  </template>
+  <div class="button-container">
+    <h1>ハムハムちゃん！</h1>
+    <router-link to="/game">
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        スタート
+      </button>
+    </router-link>
+  </div>
+</template>
 
+<!-- <script>
+new Vue({
+  el: "#app",
+  data: {
+    img: "../imgs/0-start.png",
+  },
+});
+</script> -->
 
-  <div id-"app">
-    <img v-bind:src="img" class="background-image" />
-    </div>
-  
-  <script>
-   new Vue ({
-     el: '#app',
-     data: {
-       img: '../imgs/0-start.png'
-     }
-   })
-  </script>
+<style scoped>
+h1 {
+  font-size: 3em;
+  text-align: center;
+  /* 仮です */
+}
 
-
-
-  export default {
-    methods: {
-      
-    }
-  };
-  </script>
-  <style scoped>
-  h1 {
-    font-size: 3em;
-    text-align: center;
-    /* 仮です */
-  }
-  
-   .image-container {
+.image-container {
   position: relative;
- }
+}
 
- .background-image {
-   position: absolute;
-   top: 0;
-   left: 0;
-   width: 100%;
-   height: 100%;
-  }
-  .button-container {
-    display: flex;
-    justify-content: center;
-  }
-  </style>
+.background-image {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+.button-container {
+  display: flex;
+  justify-content: center;
+}
+</style>
