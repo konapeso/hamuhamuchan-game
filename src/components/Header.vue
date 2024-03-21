9c106b9a41aa30f7c7976ca3fc80e6b0ad95df71
konapeso <n2kgt1@gmail.com>
2024-03-21 17:04:06 +0900

    routerの設定を変更

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,21 @@
 <template>
   <header class="mb-auto">
-    <nav class="flex items-center justify-between flex-wrap bg-green-200 p-6" @click="goToHome" :style="{ backgroundColor: backgroundColor }">
-      <div class="flex items-center flex-shrink-0 text-black mr-6">
-        <span class="font-semibold text-xl tracking-tight hover:text-gray-600">{{ title }}</span>
-      </div>
+    <nav
+      class="fixed top-0 left-0 w-full z-10 bg-green-200 p-6"
+      :style="{ backgroundColor: backgroundColor }"
+    >
+      <router-link
+        to="/"
+        class="flex items-center flex-shrink-0 text-gray-800 mr-6"
+      >
+        <span
+          class="font-semibold text-xl tracking-tight hover:text-gray-600"
+          >{{ title }}</span
+        >
+      </router-link>
     </nav>
   </header>
 </template>
-
 
 <script>
 export default {
@@ -15,27 +23,8 @@ export default {
   data() {
     return {
       title: "仁義なきハムハムちゃん",
-      backgroundColor: "#D6E5CB" // 初期状態の背景色を指定
+      backgroundColor: "#D6E5CB", // 初期状態の背景色を指定
     };
   },
-  methods: {
-    goToHome() {
-      // ルーターを使用してホーム画面に遷移
-      this.$router.push('/');
-      alert("ホーム画面に遷移します");
-    }
-  }
 };
 </script>
-
-
-
-
-  
-
-
-
-
-
-
-
