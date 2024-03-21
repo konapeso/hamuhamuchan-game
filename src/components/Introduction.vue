6ab9506774ba198ee5da668deeef139a0b4e4099
kayokaw <kawaikayoko@hotmail.co.jp>
2024-03-21 23:54:11 +0900

    導入ページ

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -1,14 +1,13 @@
 <template>
     <div class="flex flex-col justify-center h-screen">
       <div class="sm:max-w-full md:w-3/5 mx-auto relative">
-        <img :src="startImage" alt="スタート！" />
+        <img :src="startImage" alt="" />
   
         <div class="absolute bottom-3 left-0 right-0 flex justify-center">
           <router-link to="/game">
             <button
-              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            >
-              スタート
+              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              ゲームへ進む
             </button>
           </router-link>
         </div>
