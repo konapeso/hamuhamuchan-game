6615905a8ee18c70cf19f94444d0218af51ff509
konapeso <n2kgt1@gmail.com>
2024-03-22 22:04:33 +0900

    シェアボタン追加

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="mb-auto">
     <nav
-      class="fixed top-0 left-0 w-full z-10 bg-green-200 md:p-6 p-4"
+      class="fixed top-0 left-0 w-full z-10 bg-green-200 md:p-6 p-4 flex justify-between items-center"
       :style="{ backgroundColor: backgroundColor }"
     >
       <router-link
@@ -13,18 +13,51 @@
           >{{ title }}</span
         >
       </router-link>
+
+      <div class="flex items-center space-x-4">
+        <p class="text-gray-800 text-sm mb-1">友達にシェアしよう！</p>
+        <a :href="xURL" target="_blank" rel="noopener noreferrer">
+          <img :src="xIcon" alt="X" class="w-9 h-9" />
+        </a>
+        <a :href="lineURL" target="_blank" rel="noopener noreferrer">
+          <img :src="lineIcon" alt="LINE" class="w-10 h-10" />
+        </a>
+      </div>
     </nav>
   </header>
 </template>
 
 <script>
+import XIcon from "@/assets/icons/x-icon.png";
+import LineIcon from "@/assets/icons/line-icon.png";
+import ogImage from "@/assets/images/0-start-min.png";
+
 export default {
   name: "Header",
   data() {
     return {
       title: "仁義なきハムハムちゃん",
-      backgroundColor: "#D6E5CB", // 初期状態の背景色を指定
+      backgroundColor: "#D6E5CB",
     };
+  },
+  computed: {
+    url() {
+      return window.location.href;
+    },
+    xURL() {
+      return `https://twitter.com/intent/tweet?url=${this.url}&text=${this.title}&image=${ogImage}`;
+    },
+
+    lineURL() {
+      return `https://line.me/R/msg/text/?${this.title}%0D%0A${this.url}`;
+    },
+
+    xIcon() {
+      return XIcon;
+    },
+    lineIcon() {
+      return LineIcon;
+    },
   },
 };
 </script>
