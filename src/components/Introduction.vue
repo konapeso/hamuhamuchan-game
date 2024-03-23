9f3ce92eb67d89eb0f6c1d5ddf350b44814f4814
konapeso <n2kgt1@gmail.com>
2024-03-23 13:05:05 +0900

    透過削除

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -26,7 +26,7 @@
       <div class="text-center">
         <router-link to="/game">
           <button
-            class="text-yellow-500 border-2 border-yellow-500 font-semibold bg-white hover:bg-yellow-500 hover:text-white opacity-90 py-2 px-6 mb-3 rounded transition-colors duration-300"
+            class="text-yellow-500 border-2 border-yellow-500 font-semibold bg-white hover:bg-yellow-500 hover:text-white py-2 px-6 mb-3 rounded transition-colors duration-300"
           >
             進む
           </button>
--- src/components/StartPage.vue
+++ src/components/StartPage.vue
@@ -6,7 +6,7 @@
       <div class="absolute bottom-3 left-0 right-0 flex justify-center">
         <router-link to="/introduction">
           <button
-            class="text-yellow-500 border-2 border-yellow-500 font-semibold bg-white hover:bg-yellow-500 hover:text-white opacity-90 py-2 px-6 mb-3 rounded transition-colors duration-300"
+            class="text-yellow-500 border-2 border-yellow-500 font-semibold bg-white hover:bg-yellow-500 hover:text-white py-2 px-6 mb-3 rounded transition-colors duration-300"
           >
             スタート
           </button>
