eb55d92b667f61a0d0ec4b8efa37fd11b2b8121a
kayokaw <kawaikayoko@hotmail.co.jp>
2024-03-24 00:52:40 +0900

    アイコン修正２

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -18,10 +18,10 @@
 
       <div class="flex items-center space-x-2 md:space-x-4">
         <p class="text-gray-800 text-xs md:text-sm">友達にシェアしよう！</p>
-        <a :href="xURL" target="_blank" rel="noopener noreferrer" style="display: inline-block; max-width: 10px; max-height: 10px;">
+        <a :href="xURL" target="_blank" rel="noopener noreferrer">
          <img :src="xIcon" alt="X" class="object-contain w-full h-full" />
         </a>
-        <a :href="lineURL" target="_blank" rel="noopener noreferrer" style="display: inline-block; max-width: 10px; max-height: 10px;">
+        <a :href="lineURL" target="_blank" rel="noopener noreferrer">
          <img :src="lineIcon" alt="LINE" class="object-contain w-full h-full" />
         </a>
       </div>
