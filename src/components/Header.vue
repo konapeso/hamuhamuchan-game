3aa5b4fa2a6dd9b33f66fbce17352e2b70c1a710
kayokaw <kawaikayoko@hotmail.co.jp>
2024-03-24 14:56:22 +0900

    テスト

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,7 +9,7 @@
       </router-link>
 
       <div class="flex items-center space-x-2 md:space-x-4">
-        <p class="text-gray-800 text-xs md:text-sm">友達にシェアしよう！</p>
+        <p class="text-gray-800 text-xs md:text-sm">友達にシェアしよ！</p>
         <a :href="xURL" target="_blank" rel="noopener noreferrer">
           <img :src="xIcon" alt="X" class="w-8 h-8 md:w-9 md:h-9" />
         </a>
