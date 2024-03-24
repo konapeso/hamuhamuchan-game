7cb51552fd09160ebb2b3a6cc9bc8e1b136a92f3
kayokaw <kawaikayoko@hotmail.co.jp>
2024-03-24 15:01:33 +0900

    もとに戻す

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -18,11 +18,11 @@
 
       <div class="flex items-center space-x-2 md:space-x-4">
         <p class="text-gray-800 text-xs md:text-sm">友達にシェアしよう！</p>
-        <a :href="xURL" target="_blank" rel="noopener noreferrer" style="display: inline-block; max-width: 30px; max-height: 30px;">
-         <img :src="xIcon" alt="X" class="object-contain w-full h-full" />
+        <a :href="xURL" target="_blank" rel="noopener noreferrer">
+          <img :src="xIcon" alt="X" class="w-8 h-8 md:w-9 md:h-9" />
         </a>
-        <a :href="lineURL" target="_blank" rel="noopener noreferrer" style="display: inline-block; max-width: 30px; max-height: 30px;">
-         <img :src="lineIcon" alt="LINE" class="object-contain w-full h-full" />
+        <a :href="lineURL" target="_blank" rel="noopener noreferrer">
+          <img :src="lineIcon" alt="LINE" class="w-9 h-9 md:w-10 md:h-10" />
         </a>
       </div>
     </nav>
