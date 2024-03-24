b2c9a37a354737e529d29c2a8d28efd50b1a5221
kayokaw <kawaikayoko@hotmail.co.jp>
2024-03-24 14:58:47 +0900

    もとに戻す

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,19 +2,27 @@
   <header class="mb-auto">
     <nav
       class="fixed top-0 left-0 w-full z-10 bg-green-200 p-2 md:p-4 flex justify-between items-center"
-      :style="{ backgroundColor: backgroundColor }">
-      <router-link to="/" class="flex items-center flex-shrink-0 text-gray-800 mr-6">
+      :style="{ backgroundColor: backgroundColor }"
+    >
+      <router-link
+        to="/"
+        class="flex items-center flex-shrink-0 text-gray-800 mr-6"
+      >
         <img :src="logoJingi" alt="仁義なき" class="w-auto h-10 md:h-12" />
-        <img :src="logoHamu" alt="ハムハムちゃん" class="w-auto h-10 md:h-12 -ml-3 -mt-3" />
+        <img
+          :src="logoHamu"
+          alt="ハムハムちゃん"
+          class="w-auto h-10 md:h-12 -ml-3 -mt-3"
+        />
       </router-link>
 
       <div class="flex items-center space-x-2 md:space-x-4">
-        <p class="text-gray-800 text-xs md:text-sm">友達にシェアしよ！</p>
-        <a :href="xURL" target="_blank" rel="noopener noreferrer">
-          <img :src="xIcon" alt="X" class="w-8 h-8 md:w-9 md:h-9" />
+        <p class="text-gray-800 text-xs md:text-sm">友達にシェアしよう！</p>
+        <a :href="xURL" target="_blank" rel="noopener noreferrer" style="display: inline-block; max-width: 30px; max-height: 30px;">
+         <img :src="xIcon" alt="X" class="object-contain w-full h-full" />
         </a>
-        <a :href="lineURL" target="_blank" rel="noopener noreferrer">
-          <img :src="lineIcon" alt="LINE" class="w-9 h-9 md:w-10 md:h-10" />
+        <a :href="lineURL" target="_blank" rel="noopener noreferrer" style="display: inline-block; max-width: 30px; max-height: 30px;">
+         <img :src="lineIcon" alt="LINE" class="object-contain w-full h-full" />
         </a>
       </div>
     </nav>
